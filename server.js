c1720bd82adf2f87f4f23d98acfc3348bf7dd057
Brandao-lang <elijahbrandao@gmail.com>
2021-06-15 12:05:05 -0400

    added post method

--- server.js
+++ server.js
@@ -8,10 +8,18 @@ let rollbar = new Rollbar({
 })
 
 const app = express()
+let students = []
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served succesfully')
+})
+
+app.post('/api/student', (req, res) => {
+    let {name} = req.body
+    name = name.trim()
+    students.push(name)
+    rollbar.log('student added successfully', {author: 'Elijah', type: 'manual'})
 })
 
 const port = process.env.PORT || 4444
